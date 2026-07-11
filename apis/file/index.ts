4dae5b9c71fa768ab373f9d13d1bee6c4c3127c8
duckzuybidan <126694297+duckzuybidan@users.noreply.github.com>
2026-07-11 22:07:36 +0700

    Revert "123"
    This reverts commit 6fd754e8d39312bf30f92adfcdc761a1fcbb411e.

--- apis/file/index.ts
+++ apis/file/index.ts
@@ -1,7 +1,6 @@
 import { getMyFilesApi } from "./get-files-api";
 import { deleteFileApi } from "./delete-file-api";
 import { updateFileApi } from "./update-file-api";
-import { openFileApi, streamFileUrl } from "./open-file-apis";
-import { getFile } from "./get-file";
+import { openFileApi } from "./open-file-apis";
 
-export { getMyFilesApi, deleteFileApi, updateFileApi, openFileApi, streamFileUrl, getFile };
+export { getMyFilesApi, deleteFileApi, updateFileApi, openFileApi };
--- apis/file/open-file-apis.ts
+++ apis/file/open-file-apis.ts
@@ -1,14 +1,8 @@
 import api from "../api-config";
-import { ACCESS_TOKEN } from "@/lib/var";
 
 export const openFileApi = async (fileId: string) => {
   const response = await api.get(`/file/open/${fileId}`, {
     responseType: "blob",
   });
   return response.data;
-};
-
-export const streamFileUrl = (fileId: string) => {
-  const token = localStorage.getItem(ACCESS_TOKEN);
-  return `/api/file/stream/${fileId}${token ? `?token=${token}` : ""}`;
 };
